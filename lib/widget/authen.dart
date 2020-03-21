b36e995ea5c551c60e559406b0eda43b9906b6e1
test <gunyaphut.s@gmail.com>
2020-03-21 11:37:58 +0700

    Create Logo Success

---
+++ lib/widget/authen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Authen extends StatefulWidget {
+  @override
+  _AuthenState createState() => _AuthenState();
+}
+
+class _AuthenState extends State<Authen> {
+  // Field
+
+  // method
+  Widget showLogo() {
+    return Center(
+      child: Container(
+        width: 100.0,
+        child: Image.asset('images/logo.png'),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: showLogo(),
+    );
+  }
+}
