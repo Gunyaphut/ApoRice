e5a77a40f0a5f6af4ba87601861e17bd749a02cc
test <gunyaphut.s@gmail.com>
2020-03-22 15:44:11 +0700

    Authen

--- lib/widget/authen.dart
+++ lib/widget/authen.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
+import 'package:aporice/utility/normal_dialog.dart';
+import 'package:aporice/widget/my_service.dart';
 import 'package:aporice/widget/register.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -9,24 +14,73 @@ class Authen extends StatefulWidget {
 
 class _AuthenState extends State<Authen> {
   // Field
+  String user, password;
 
   // method
   Widget singInButton() {
     return RaisedButton(
       color: Colors.brown.shade700,
       child: Text('Sign IN'),
-      onPressed: () {},
+      onPressed: () {
+        if (user == null ||
+            user.isEmpty ||
+            password == null ||
+            password.isEmpty) {
+          normalDialog(
+              context, 'Cannot Register มีช่องว่าง', 'กรุณากรอกทุกช่อง');
+        } else {
+          checkAuthen();
+        }
+      },
     );
+  }
+
+  Future<void> checkAuthen() async {
+    try {
+      String url =
+          'https://www.androidthai.in.th/rice/getUserWhereUserApo.php?isAdd=true&User=$user';
+      var response = await Dio().get(url);
+      print('response ===>> $response');
+
+      if (response.toString() == 'null') {
+        normalDialog(context, 'User False', 'No $user in My Database');
+      } else {
+        // คือการแปลงภาษาโคตให้เป็นภาษาไทย
+        var resule = json.decode(response.data);
+        print('result ==>> $resule');
+
+//เพราะติดสแควบัคเก็ต[] ต้องทำการปรดออกไป
+        for (var map in resule) {
+          //ทำเพื่อ ส่งค่าไปอีกหน้า
+          String truePassword = map['Password'];
+          String nameLogin = map['Name'];
+
+          if (password == truePassword) {
+            //คือการเร้า คือการกดแล้วไปที่ไหน
+            MaterialPageRoute route = MaterialPageRoute(
+              builder: (context) => MyService(
+                name: nameLogin,
+              ),
+            );
+            //เมื่อกดไปแล้ว ห้ามกลับ กรณ ตัวอย่างเช่น Login
+            Navigator.of(context).pushAndRemoveUntil(route, (value) => false);
+          } else {
+            normalDialog(context, 'พาสผิด', 'กรุณาใส่รหัสใหม่');
+          }
+        }
+      }
+    } catch (e) {}
   }
 
   Widget singUpButton() {
     return OutlineButton(
       child: Text('Sign UP'),
       onPressed: () {
-        //เชื่อมให้ตัดไปอีกหน้า วิธีการทำเร้า 
+        //เชื่อมให้ตัดไปอีกหน้า วิธีการทำเร้า
         MaterialPageRoute route =
             MaterialPageRoute(builder: (value) => Register());
-            Navigator.of(context).push(route);
+            //เมื่อกดแล้วกดกลับหน้าเดิมได้
+        Navigator.of(context).push(route);
       },
     );
   }
@@ -53,6 +107,7 @@ class _AuthenState extends State<Authen> {
     return Container(
       width: 250.0,
       child: TextField(
+        onChanged: (value) => user = value.trim(),
         decoration: InputDecoration(
           labelText: 'User:',
           border: OutlineInputBorder(),
@@ -65,6 +120,7 @@ class _AuthenState extends State<Authen> {
     return Container(
       width: 250.0,
       child: TextField(
+        onChanged: (value) => password = value.trim(),
         obscureText: true, //ใช้เปิดปิดพาสเวิดให้เป็นจุดๆ
         decoration: InputDecoration(
           labelText: 'Password:',
---
+++ lib/widget/my_service.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+
+//คือการโยนค่าข้ามสเตจ 
+class MyService extends StatefulWidget {
+
+  final String name;
+  MyService({Key key, this.name}) : super(key: key);
+
+
+
+  @override
+  _MyServiceState createState() => _MyServiceState();
+}
+
+
+
+class _MyServiceState extends State<MyService> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      appBar:  AppBar(title: Text('My Service'),),
+      
+    );
+  }
+}
